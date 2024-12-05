55cd0c456b13b69feced8f38046acfa343cfb9d5
pablomesquita <pablomesquita06@gmail.com>
2024-12-05 06:26:44 -0300

    feat: Create new component input

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from './components/button/button.component';
 import { CardPopupComponent } from './components/card-popup/card-popup.component';
 import { CardSelfcarePopupComponent } from './components/card-selfcare-popup/card-selfcare-popup.component';
 import { RouterModule } from '@angular/router';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [ButtonComponent, CardPopupComponent, CardSelfcarePopupComponent],
+  declarations: [ButtonComponent, CardPopupComponent, CardSelfcarePopupComponent, InputComponent],
   imports: [CommonModule],
-  exports: [ButtonComponent, CardPopupComponent, CardSelfcarePopupComponent, RouterModule],
+  exports: [ButtonComponent, CardPopupComponent, CardSelfcarePopupComponent, RouterModule, InputComponent],
 })
 export class SharedModule {}
