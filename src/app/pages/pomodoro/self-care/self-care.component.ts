b7b1c4770f23d31ce735392c72a226d37edc5e66
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2024-12-12 19:08:50 -0300

    feat: adicionado pop-up ao finalizar o foco para redirecionar para a pagina de self-care

---
+++ src/app/pages/pomodoro/self-care/self-care.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-self-care',
+  templateUrl: './self-care.component.html',
+  styleUrl: './self-care.component.scss',
+})
+export class SelfCareComponent {}
